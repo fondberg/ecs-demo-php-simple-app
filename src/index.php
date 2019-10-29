4e31fce9eb8fc85167b1ee734828bb7c1baff86a
Niklas Fondberg <niklas.fondberg@gmail.com>
2019-10-29 11:23:32 +0100

    testing pipeline

--- src/index.php
+++ src/index.php
@@ -24,7 +24,7 @@
                         echo fread($myfile,filesize("/var/www/my-vol/date"));
                         fclose($myfile);
                 ?>
-
+                <p>NIKLAS</p>
             </div>
         </div>
 
